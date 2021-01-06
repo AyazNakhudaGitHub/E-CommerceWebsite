15054f3919c46d971fc611117d83393c33b7e810
AyazNakhudaGitHub <63979892+AyazNakhudaGitHub@users.noreply.github.com>
2021-01-06 17:25:38 -0500

    Add files via upload

---
+++ E-Commerce Grocery Store Website/grocery copy/footer/footer-bootstrap.php
@@ -0,0 +1,60 @@
+<footer>
+    <!-- Initiate the footer -->
+    <div class="footer">
+        <!-- Footer is split up in two sections, "content" and "bottom" -->
+        <div class="footer-content">
+            <!-- About section in the footer -->
+            <div class="footer-section about">
+                <!-- Logo -->
+                <span class="logo"><i class="fas fa-shopping-basket logo"></i>TheGroceryStore</span>
+                <br><br>
+                <!-- Company description -->
+                <p>
+                    <i>TheGroceryStore is an easy-to-use online grocery store where hospitality towards customers is our top priority!</i> 
+                </p>
+                <br>
+                <!-- Contact information in the footer -->
+                <div class="contact">
+                    <!-- Phone information -->
+                    <span><i class="fas fa-phone"></i>&nbsp;&nbsp;1-800-GROCERY</span>
+                    <!-- Email information -->
+                    <span><i class="fas fa-envelope"></i>&nbsp;&nbsp;info@TheGroceryStore.com</span>
+                </div>
+            </div>
+
+            <!-- Footer quick links section -->
+            <div class="footer-section links">
+                <h2>Quick Links</h2>
+                <ul>
+                    <!-- All links to other pages -->
+                    <a href="/grocery/InfoPages/about-us.php"><li><i class="fas fa-angle-double-right"></i><b> About Us</b></li></a>
+                    <a href="/grocery/ShoppingCart/cart.php"><li><i class="fas fa-angle-double-right"></i><b> Cart</b></li></a>
+                    <a href="/grocery/ConsumerTools/locator.php"><li><i class="fas fa-angle-double-right"></i><b> Store Locator</b></li></a>
+                    <a href="/grocery/ConsumerTools/nutritionalPage.php"><li><i class="fas fa-angle-double-right"></i><b> Nutritional Info</b></li></a>
+                    <a href="/grocery/InfoPages/return-policy.php"><li><i class="fas fa-angle-double-right"></i><b> Return Policy</b></li></a>
+                    <a href="/grocery/InfoPages/terms-of-service.php"><li><i class="fas fa-angle-double-right"></i><b> Terms of Service</b></li></a>
+                </ul>
+            </div>
+
+            <!-- COVID-19 tips in the footer -->
+            <div class="footer-section tips">
+                <h2>COVID-19 Tips</h2>
+                <ul>
+                    <!-- The tips -->
+                    <li><i class="fas fa-info-circle"></i> Wipe down any groceries</li>
+                    <li><i class="fas fa-info-circle"></i> Wash your hands regularly</li>
+                    <li><i class="fas fa-info-circle"></i> Do not touch your eyes, nose or mouth</li>
+                    <li><i class="fas fa-info-circle"></i> Eat healthy and keep strong</li>
+                    <li><i class="fas fa-info-circle"></i> Don't forget about mental health</li>
+
+                </ul>
+            </div>
+            
+        </div>
+        <!-- The bottom part of the footer -->
+        <div class="footer-bottom">
+            <!-- Copyright and Authors -->
+            &copy; TheGroceryStore | Designed by David Ferris, Ayaz Nakhuda & Muhammad Alvi
+        </div>
+    </div>
+</footer>
